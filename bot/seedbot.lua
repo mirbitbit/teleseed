04e4047fe2360928fe76a6ccffa40bd5bfa89f8a
mirbitbit <mirbitbit@gmail.com>
2015-12-20 19:43:26 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,26 +220,20 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {110626080,103649648,0,tonumber(our_id)},--Sudo users
+    sudo_users = {178263682,141996114,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v1
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
+👿😈
 
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@seyedan25 [Manager]
+@amir_ab_1
+@mas1h
 
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
+MIRSMARTBOT
 Our channels
 @teleseedch [English]
 ]],
